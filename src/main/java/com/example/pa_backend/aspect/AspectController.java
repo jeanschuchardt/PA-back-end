8eb34773c729bb0408b7b15246c9d176ef9d8583
Jean <jean@sherpaerp.com>
2022-11-11 23:12:13 -0300

    ok

---
+++ src/main/java/com/example/pa_backend/aspect/AspectController.java
@@ -0,0 +1,31 @@
+package com.example.pa_backend.aspect;
+
+import lombok.extern.apachecommons.CommonsLog;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@CommonsLog
+public class AspectController {
+
+    @Pointcut("execution(public * com.example.pa_backend.controller.*.*(..))")
+    public void pointCut() {
+    }
+
+    @Around("pointCut()")
+    public Object aroundHandle(ProceedingJoinPoint pjd) {
+        String action = pjd.getSignature().getName();
+        log.info(action);
+        try {
+            return pjd.proceed();
+        } catch (Throwable e) {
+            log.error(e.getMessage(), e);
+            throw  new RuntimeException();
+        }
+
+    }
+}
