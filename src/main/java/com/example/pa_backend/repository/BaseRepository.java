6480c99616ca3e7872f017f1e180822f4c8c56c0
Jean <jean@sherpaerp.com>
2022-11-19 19:05:03 -0300

    base repository

--- src/main/java/com/example/pa_backend/repository/BaseRepository.java
+++ src/main/java/com/example/pa_backend/repository/BaseRepository.java
@@ -1,22 +1,15 @@
 package com.example.pa_backend.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import java.util.List;
 import java.util.Optional;
 
 @NoRepositoryBean
 public interface BaseRepository<T> extends JpaRepository<T, Integer>, JpaSpecificationExecutor<T> {
 
-    Optional<T> findByIdAndIsArchived(Integer id, Integer isArchived);
+    Optional<T> findById(Integer id);
 
-    List<T> findByIdInAndIsArchived(List<Integer> ids, Integer isArchived);
 
-    List<T> findAllByIsArchived(Integer isArchived);
-    Page<T> findAllByIsArchived(Integer isArchived, Pageable pageable);
-    
 }
