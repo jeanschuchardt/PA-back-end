4693bad9fee797555fc21a11a0b10b6f2f6bfa17
Jean <jean@sherpaerp.com>
2022-11-19 19:31:48 -0300

    create controller

--- src/main/java/com/example/pa_backend/controller/TherapistController.java
+++ src/main/java/com/example/pa_backend/controller/TherapistController.java
@@ -1,9 +1,40 @@
 package com.example.pa_backend.controller;
 
 import lombok.extern.apachecommons.CommonsLog;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
 @CommonsLog
+@RestController
+@RequestMapping("/therapist")
 public class TherapistController {
+
+    @GetMapping("")
+    public Object getAll() {
+
+        return "";
+    }
+
+    @GetMapping("/{id}")
+    public Object getById(@PathVariable int id) {
+
+        return null;
+    }
+
+    @PostMapping("/")
+    public Object create() {
+
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Object update(@PathVariable int id) {
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Object delete(@PathVariable int id) {
+
+        return null;
+    }
 }
