1453582f931d6c4e729089ba3a7eefc5ccecb82c
Jean <jean@sherpaerp.com>
2022-12-04 17:32:09 -0300

    base entity should be abstract

--- src/main/java/com/example/pa_backend/entity/BaseEntity.java
+++ src/main/java/com/example/pa_backend/entity/BaseEntity.java
@@ -2,17 +2,15 @@ package com.example.pa_backend.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
-@Entity
-public class BaseEntity {
+@MappedSuperclass
+public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
 }
