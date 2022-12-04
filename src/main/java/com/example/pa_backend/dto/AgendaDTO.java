7ce8207fd6c6ea4193dece204434d32128834d5b
Jean <jean@sherpaerp.com>
2022-12-04 17:32:34 -0300

    Revert "agenda"
    This reverts commit 1237b61ce76b1d0a5a3d8385dacb842993b5e049.

--- src/main/java/com/example/pa_backend/dto/AgendaDTO.java
+++ src/main/java/com/example/pa_backend/dto/AgendaDTO.java
@@ -3,17 +3,15 @@ package com.example.pa_backend.dto;
 import lombok.Data;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Date;
 
 
 @Data
 public class AgendaDTO  {
 
-    private LocalDate date;
-    private LocalTime startTime;
-    private LocalTime endTime;
+    private Date date;
+    private Timestamp startTime;
+    private Timestamp endTime;
     private int  therapistId;
     private int  clientId;
     private int locationId;
--- src/main/java/com/example/pa_backend/entity/Agenda.java
+++ src/main/java/com/example/pa_backend/entity/Agenda.java
@@ -6,23 +6,20 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Date;
 
 @Entity(name = "agenda")
 @Data
-@Table(name = "agenda")
 public class Agenda extends BaseEntity {
 
     @Column
-    private LocalDate date;
+    private Date date;
 
     @Column
-    private LocalTime startTime;
+    private Timestamp startTime;
 
     @Column
-    private LocalTime endTime;
+    private Timestamp endTime;
 
     @Column
     private int  therapistId;
