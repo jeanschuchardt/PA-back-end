4fed2e03529336c15499d28385ad22970b3b7827
Jean <jean@sherpaerp.com>
2022-12-04 17:33:44 -0300

    Revert "Revert "agenda""
    This reverts commit 7ce8207fd6c6ea4193dece204434d32128834d5b.

--- src/main/java/com/example/pa_backend/dto/AgendaDTO.java
+++ src/main/java/com/example/pa_backend/dto/AgendaDTO.java
@@ -3,15 +3,17 @@ package com.example.pa_backend.dto;
 import lombok.Data;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
 
 
 @Data
 public class AgendaDTO  {
 
-    private Date date;
-    private Timestamp startTime;
-    private Timestamp endTime;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
     private int  therapistId;
     private int  clientId;
     private int locationId;
